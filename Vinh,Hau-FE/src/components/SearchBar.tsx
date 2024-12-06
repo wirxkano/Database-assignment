c80f265860ec9c150ba0f3e6718ac14984e3fe29
wirxkano <159103353+wirxkano@users.noreply.github.com>
2024-12-06 23:13:48 +0700

    Delete Vinh,Hau-FE directory

--- Vinh,Hau-FE/src/components/SearchBar.tsx
+++
@@ -1,9 +0,0 @@
-import { Input } from "@/components/ui/input"
-
-const SearchBar = () => {
-  return (
-    <Input type="text" placeholder="You can search for Order's ID..." />
-  )
-}
-
-export default SearchBar
