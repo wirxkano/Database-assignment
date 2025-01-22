d1c61f5260cc65b6f277984395336edc25f4abb0
thudngm <dngmtthu@gmail.com>
2025-01-22 21:07:59 +0700

    [b] solve conflict

--- Ecommerce_BE/src/config/env.js
+++ Ecommerce_BE/src/config/env.js
@@ -4,11 +4,15 @@ import 'dotenv/config';
 export const env = {
   APP_HOST: process.env.APP_HOST,
   APP_PORT: process.env.APP_PORT,
+
   DB_HOST: process.env.DB_HOST,
   DB_PORT: process.env.DB_PORT,
   DB_USERNAME: process.env.DB_USERNAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_NAME: process.env.DB_NAME,
+
   BUILD_MODE: process.env.BUILD_MODE,
   SECRET_ACCESS_TOKEN: process.env.SECRET_ACCESS_TOKEN
 };
+
+console.log("env calling: ", env);
--- Ecommerce_BE/src/server.js
+++ Ecommerce_BE/src/server.js
@@ -1,27 +1,27 @@
 /* eslint-disable no-console */
-import express from 'express';
-import { env } from '~/config/env';
-import { API } from '~/routes/index';
-import { connectToDB } from '~/config/connectDB';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import { env } from "./config/env";
+import { API } from "~/routes/index";
+import { connectToDB } from "~/config/connectDB";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const SERVER = () => {
   const app = express();
 
   let corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: "http://localhost:5173",
     optionsSuccessStatus: 200,
-    credentials: true
+    credentials: true,
   };
 
   app.use(cors(corsOptions));
   app.use(cookieParser());
 
   app.use(express.json());
-  app.use('/api', API);
+  app.use("/api", API);
 
-  if (env.BUILD_MODE == 'dev') {
+  if (env.BUILD_MODE == "dev") {
     app.listen(env.APP_PORT, () => {
       console.log(`Running on: http://${env.APP_HOST}:${env.APP_PORT}/api`);
     });
@@ -29,6 +29,6 @@ const SERVER = () => {
 };
 
 connectToDB()
-  .then(() => console.log('Connect to SQL Server successfully!'))
+  .then(() => console.log("Connect to SQL Server successfully!"))
   .then(() => SERVER())
   .catch((err) => console.log(err));
